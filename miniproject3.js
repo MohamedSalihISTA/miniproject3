8c747b89466e57c4e3403442544ed0daf889cd7b
MohamedSalihISTA <159767038+MohamedSalihISTA@users.noreply.github.com>
2024-03-31 21:02:32 +0000

    Add files via upload

---
+++ miniproject3.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navList = document.querySelector('.navigation ul');
+    navList.classList.toggle('open');
+  });
+  
